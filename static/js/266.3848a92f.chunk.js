6ecc4c3a5d80bf261c39cfb68351d15a10608d17
giligran <118477139+giligran@users.noreply.github.com>
2023-09-12 16:56:25 +0000

    Deploying to gh-pages from @ giligran/goit-react-hw-08-phonebook@8b8663053312d81c41e5ef25d53cf4b7bc479bc0 🚀

---
+++ static/js/266.3848a92f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var t=o(184);function c(){return(0,t.jsx)("div",{children:(0,t.jsx)("h1",{children:"Phone book welcome page"})})}}}]);
+//# sourceMappingURL=266.3848a92f.chunk.js.map
